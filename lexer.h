17b5c03fd2e73dcc6b8cb949d7041599fcba60aa
Ryuta <1457218065@qq.com>
2022-09-22 20:33:53 +0800

    defineVer

---
+++ lexer.h
@@ -0,0 +1,25 @@
+#ifndef LEXCIAL_H
+#define LEXCIAL_H
+#include<string>
+using namespace std;
+class Token
+{
+private:
+
+public:
+};
+class Tuple
+{
+    private:
+        string type;
+        string name;
+    public:
+};
+class Lexer
+{
+private:
+public:
+    lexer();
+};
+
+#endif
