2d5fc9d8896202b21b13f815c19acac91359bf6c
Ryuta <1457218065@qq.com>
2022-09-23 16:56:05 +0800

    yyy

--- lexer.h
+++ lexer.h
@@ -1,25 +1,62 @@
 #ifndef LEXCIAL_H
 #define LEXCIAL_H
 #include<string>
+#include<list>
+#include<fstream>
 using namespace std;
+
 class Token
 {
 private:
-
+    string name;
+    string value;
+    string type;
 public:
+    Token();
+
+    string getName() const { return name; }
+    void setName(const string &name_) { name = name_; }
+
+    string getType() const { return type; }
+    void setType(const string &type_) { type = type_; }
+
+    Token(string n,string t,string value);
+
+    string getValue() const { return value; }
+    void setValue(const string &value_) { value = value_; }
+    
+    string toString();
+    
 };
-class Tuple
+class TokenTable
 {
     private:
-        string type;
-        string name;
+    
+        list<Token> tokens;
     public:
+        TokenTable();
+        void add(const Token &);
+        void show();
+        bool find(const string &s)const;
+        
 };
+
 class Lexer
 {
 private:
+    int currentRow;
+    int currentColum;
+    TokenTable tokTable;
+    char pointer;
+
 public:
-    lexer();
+    Lexer();
+    pair<string,string> getNextToken(istream &);
+    static bool isDigit(char &);
+    static bool isCharacter(char &);
+    int getCurrentRow()const;
+    int getCurrentColum()const;
+    TokenTable getTokenTable()const;
 };
 
 #endif
